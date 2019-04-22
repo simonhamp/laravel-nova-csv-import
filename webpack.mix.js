2731b023916d0fe11b2c74af84e4da825b3e27e8
Simon Hamp <simon.hamp@me.com>
2019-04-22 16:31:31 +0100

    Fix JS build

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,4 @@ let mix = require('laravel-mix')
 
 mix.setPublicPath('dist')
     .js('resources/js/tool.js', 'js')
-    .sass('resources/sass/tool.scss', 'css')
+    //.sass('resources/sass/tool.scss', 'css')
