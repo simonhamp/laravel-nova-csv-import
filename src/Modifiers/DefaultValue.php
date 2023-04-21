79763538123d9dc2f1b59b4fb0c2d1499b3395d2
Kevin Daniel <kevin.dapps@gmail.com>
2023-04-21 13:59:50 -0400

    Create default value modifier

---
+++ src/Modifiers/DefaultValue.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace SimonHamp\LaravelNovaCsvImport\Modifiers;
+
+use SimonHamp\LaravelNovaCsvImport\Contracts\Modifier;
+
+class DefaultValue implements Modifier
+{
+  public function title(): string
+  {
+    return 'Default Value';
+  }
+
+  public function description(): string
+  {
+    return "Set a default value for the field if the CSV column is empty or missing";
+  }
+
+  public function settings(): array
+  {
+    return [
+      'string' => [
+        'type' => 'string',
+        'title' => 'Default Value',
+      ],
+    ];
+  }
+
+  public function handle($value = null, array $settings = []): string
+  {
+    return empty($value) ? $settings['string'] : $value;
+  }
+}
