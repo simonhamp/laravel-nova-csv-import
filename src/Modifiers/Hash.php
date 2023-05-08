9dc4e258eeb47e2f88b69cbe370898d30f8b415d
kerbs-ns <120722197+kerbs-ns@users.noreply.github.com>
2023-05-08 10:56:54 +0200

    Adding laravel hash support
    One can select laravel.password that would use default Hash::make driver in project
    
    Or specify whitch to use:
    - laravel.bcrypt
    - laravel.argon
    - laravel.argon2id

--- src/Modifiers/Hash.php
+++ src/Modifiers/Hash.php
@@ -2,7 +2,10 @@
 
 namespace SimonHamp\LaravelNovaCsvImport\Modifiers;
 
+use Exception;
+use Illuminate\Support\Facades\Hash as LaravelHashFascade;
 use SimonHamp\LaravelNovaCsvImport\Contracts\Modifier;
+use Illuminate\Support\Str;
 
 class Hash implements Modifier
 {
@@ -19,12 +22,42 @@ class Hash implements Modifier
     public function settings(): array
     {
         return [
-            'algorithm' => hash_algos(),
+            'algorithm' => array_merge(
+                [
+                    'laravel.password',
+                    'laravel.bcrypt',
+                    'laravel.argon',
+                    'laravel.argon2id'
+                ],
+                hash_algos()
+            ),
         ];
     }
 
     public function handle($value = null, array $settings = []): string
     {
+        if (Str::startsWith($settings['algorithm'], 'laravel.')) {
+            return $this->handleLaravelHash($value, $settings);
+        }
+
         return hash($settings['algorithm'], $value);
+    }
+
+    public function handleLaravelHash($value = null, array $settings = []): string
+    {
+        switch ($settings['algorithm']) {
+            case 'laravel.password':
+                return LaravelHashFascade::make($value);
+                break;
+            case 'laravel.bcrypt':
+                return LaravelHashFascade::driver('bcrypt')->make($value);
+                break;
+            case 'laravel.argon':
+                return LaravelHashFascade::driver('argon')->make($value);
+                break;
+            case 'laravel.argon2id':
+                return LaravelHashFascade::driver('argon2id')->make($value);
+                break;
+        }
     }
 }
