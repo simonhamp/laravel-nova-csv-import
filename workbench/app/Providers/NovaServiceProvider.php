dd92fdf17102473e28b6d87dc63461df65346bf2
Mior Muhammad Zaki <crynobone@gmail.com>
2023-09-01 20:12:51 +0800

    Workbench Integration
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ workbench/app/Providers/NovaServiceProvider.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Workbench\App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use NovaKit\NovaDevTool\Nova;
+use Laravel\Nova\NovaApplicationServiceProvider;
+use Orchestra\Workbench\Workbench;
+
+class NovaServiceProvider extends NovaApplicationServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+    }
+
+    /**
+     * Register the Nova routes.
+     *
+     * @return void
+     */
+    protected function routes()
+    {
+        Nova::routes()
+            ->withAuthenticationRoutes()
+            ->withPasswordResetRoutes()
+            ->register();
+    }
+
+    /**
+     * Register the Nova gate.
+     *
+     * This gate determines who can access Nova in non-local environments.
+     *
+     * @return void
+     */
+    protected function gate()
+    {
+        Gate::define('viewNova', function ($user) {
+            return true;
+        });
+    }
+
+    /**
+     * Get the dashboards that should be listed in the Nova sidebar.
+     *
+     * @return array
+     */
+    protected function dashboards()
+    {
+        return [
+            new \Laravel\Nova\Dashboards\Main(),
+        ];
+    }
+
+    /**
+     * Get the tools that should be listed in the Nova sidebar.
+     *
+     * @return array
+     */
+    public function tools()
+    {
+        return [
+            new \SimonHamp\LaravelNovaCsvImport\LaravelNovaCsvImport(),
+        ];
+    }
+
+    /**
+     * Register the application's Nova resources.
+     *
+     * @return void
+     */
+    protected function resources()
+    {
+        Nova::resourcesIn(Workbench::path('app/Nova'));
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
